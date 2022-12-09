c3fca4bae621c1d83c5935683f2b2bfb6a600e24
Jordan McMichael <jlmcmchl@gmail.com>
2022-12-09 01:27:08 -0500

    day 7 faster

--- src/day07/input.rs
+++ src/day07/input.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use nom::{
     branch::alt,
     bytes::complete::tag,
@@ -10,7 +12,13 @@ use nom::{
 
 use crate::day07::Input;
 
-use super::Line;
+#[derive(Debug, Clone)]
+pub enum Line {
+    CD(String),
+    LS,
+    File(String, usize),
+    Directory(String),
+}
 
 const INPUT: &str = include_str!("../../input/07/input.txt");
 
@@ -43,11 +51,71 @@ fn parse_file(input: &str) -> IResult<&str, Line> {
     Ok((input, Line::File(name.into(), size.parse().unwrap())))
 }
 
-pub fn read() -> Input {
+fn parse_input() -> Vec<Line> {
     let result = separated_list1(
         line_ending,
         alt((parse_cd, parse_ls, parse_dir, parse_file)),
     )(INPUT);
 
     result.unwrap().1
+}
+
+fn construct_filesystem(input: &[Line]) -> Input {
+    let mut subfolders = HashMap::new();
+    let mut stack = Vec::new();
+
+    let mut idx = 0;
+
+    while idx < input.len() {
+        match &input[idx] {
+            Line::CD(dir) => match dir.as_str() {
+                "/" => {
+                    stack.clear();
+                    stack.push("/".into())
+                }
+                ".." => {
+                    stack.pop();
+                }
+                dir => stack.push(dir.to_owned()),
+            },
+            Line::LS => {
+                let mut content = Vec::new();
+                let mut inner_idx = idx + 1;
+                while inner_idx < input.len()
+                    && matches!(&input[inner_idx], Line::File(..) | Line::Directory(..))
+                {
+                    if let Line::Directory(name) = &input[inner_idx] {
+                        content.push(full_name(&stack) + "/" + name);
+                    }
+                    inner_idx += 1;
+                }
+
+                subfolders.insert(full_name(&stack), (0, content));
+            }
+            Line::Directory(name) => {
+                // sizes.insert(full_name(&stack) + "/" + name, 0);
+            }
+            Line::File(name, size) => {
+                // sizes.insert(full_name(&stack) + "/" + name, *size);
+
+                subfolders
+                    .entry(full_name(&stack))
+                    .and_modify(|val| val.0 += *size);
+            }
+        }
+
+        idx += 1;
+    }
+
+    subfolders
+}
+
+pub fn read() -> Input {
+    let inp = parse_input();
+
+    construct_filesystem(&inp)
+}
+
+fn full_name(stack: &[String]) -> String {
+    stack.iter().cloned().reduce(|a, b| a + "/" + &b).unwrap()
 }
--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -2,9 +2,11 @@ pub mod input;
 pub mod part1;
 pub mod part2;
 
+use std::collections::HashMap;
+
 use crate::{Output, Part};
 
-pub type Input = Vec<Line>;
+pub type Input = (HashMap<String, (usize, Vec<String>)>);
 
 pub fn run(part: Part) -> Output {
     let input = input::read();
@@ -12,6 +14,17 @@ pub fn run(part: Part) -> Output {
         Part::One => part1::solve(&input),
         Part::Two => part2::solve(&input),
     }
+}
+
+fn folder_size(folder: &str, hierarchy: &HashMap<String, (usize, Vec<String>)>) -> usize {
+    let (total, subfolders) = &hierarchy[folder];
+    let mut total = *total;
+
+    for subfolder in subfolders {
+        total += folder_size(subfolder, hierarchy);
+    }
+
+    total
 }
 
 #[cfg(test)]
@@ -29,12 +42,4 @@ mod tests {
         let result = run(Part::Two);
         println!("{result}");
     }
-}
-
-#[derive(Debug, Clone)]
-pub enum Line {
-    CD(String),
-    LS,
-    File(String, usize),
-    Directory(String),
 }
